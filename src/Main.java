b1862635d13101144c0db3924a6415cc8c686089
piokowalski <piokowalski@onet.pl>
2019-03-24 22:18:31 +0100

    Section 4.30 - Methods - how to use - basics

--- src/Main.java
+++ src/Main.java
@@ -20,10 +20,27 @@ public class Main {
         myVariable--;
 
         //if keywords and code blocks
+
         boolean gameOver = true;
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
+
+        //Using same variables used before, but what is in the Scope {} it remains there ;)
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " +finalScore);
+        }
+
+        // We're using method from below just here (instead of typing all these lines)
+
+        calculateScore();
+
+        calculateScoreShortMethod(true, 800, 4,100);
 
 //        if (score < 5000 && > 1000) {
 //            System.out.println("Your score was less than 5000" +
@@ -33,21 +50,34 @@ public class Main {
 //        } else {
 //            System.out.println("Got here");
 //        }
+//
+//        if(gameOver == true) {
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
+
+        //   M   E   T   H   O   D   S
+    }
+    // methods should be outside the main() method
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
 
         if(gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
-        //Using same variables used before, but what is in the Scope {} it remains there ;)
-       score = 10000;
-       levelCompleted = 8;
-       bonus = 200;
-
+    }
+        //SHORTER WAY - using parameters in the brackets ( , , ) and returning int finalScore
+    public static int calculateScoreShortMethod(boolean gameOver, int score, int levelCompleted, int bonus) {
         if(gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " +finalScore);
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
-
-
     }
+
 }
